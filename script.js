c4821ff88674c461ee58e1222962a1d118c9c09f
ofonimeedak <ofonimeedak@gmail.com>
2026-06-07 08:59:41 +0100

    summary text

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ function createEpisodeCard(episode) {
     episode.image?.medium || "placeholder.png";
 
   card.querySelector(".episode-summary").innerHTML =
-    episode.summary.trim().slice(0,50) || "No summary available.";
+    episode.summary.trim()|| "No summary available.";
 
   return card;
 }
